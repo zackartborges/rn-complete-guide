d2bc53f6bf8baec6130308ec34fcc3af5e003569
zackartborges <zackartborges@gmail.com>
2021-11-08 14:26:41 -0500

    adds scrollview, then transitions scrollview into flatList

--- App.js
+++ App.js
@@ -1,32 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
-  const [courseGoals, setCourseGoals] = useState([])
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
-      setEnteredGoal(enteredText);
-  }
+    setEnteredGoal(enteredText);
+  };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
-  }
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredGoal },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput 
-        placeholder="Course Goals"
-        style={styles.input}
-        onChangeText={goalInputHandler}
-        value={enteredGoal}
+        <TextInput
+          placeholder="Course Goals"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Add" onPress={addGoalHandler}/>
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View >
-        {courseGoals.map((goal) => <Text style= {styles.listItem} key={goal}>{goal}</Text>)}
-      </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -34,26 +51,26 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-  borderColor: 'black',
-  borderWidth: 1, 
-  padding: 50,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 50,
   },
   inputContainer: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   input: {
-    width: '80%',
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10 
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
   },
   listItem: {
-    padding: 10, 
+    padding: 10,
     marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
+    backgroundColor: "#ccc",
+    borderColor: "black",
     borderEndWidth: 1,
-  }
+  },
 });
